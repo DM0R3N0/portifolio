124b9a00287e768dac8dadb97d71c10fef4c2330
DM0R3N0 <dfm84@hotmail.com>
2021-11-13 13:46:44 -0300

    Update

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+alert(`Obrigado pela visita. O Site estÃ¡ em desenvolvimento !!! 
+Se desejar clique em OK e acompanhe a evoluÃ§Ã£o`)
+
+
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
@@ -6,23 +10,23 @@ document.querySelector("#qtde").addEventListener("change", atualizarPreco)
 document.querySelector("#js").addEventListener("change", atualizarPreco)
 document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
 document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
-document.querySelector("#prazo").addEventListener("change", function () {
+document.querySelector("#prazo").addEventListener("change", function() {
     const prazo = document.querySelector("#prazo").value
     document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
     atualizarPreco()
 })
 
-function atualizarPreco(){
+function atualizarPreco() {
     const qtde = document.querySelector("#qtde").value
     const temJS = document.querySelector("#js").checked
     const incluiLayout = document.querySelector("#layout-sim").checked
     const prazo = document.querySelector("#prazo").value
-  
-    let preco = qtde * 100;
-    if (temJS) preco *= 1.1
-    if (incluiLayout) preco += 500
-    let taxaUrgencia = 1 - prazo*0.1;
+
+    let preco = qtde * 300;
+    if (temJS) preco *= 1.50
+    if (incluiLayout) preco += 800
+    let taxaUrgencia = 1 - prazo * 0.1;
     preco *= 1 + taxaUrgencia
-   
+
     document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
 }
